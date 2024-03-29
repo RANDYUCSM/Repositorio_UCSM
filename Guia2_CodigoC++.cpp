d3a8e317f0fbe7ca191ee9748d241db74e2848a5
JesusG. <condorigustavo45@gmail.com>
2024-03-29 15:00:04 -0500

    Actividad 4:3

--- Guia2_CodigoC++.cpp
+++ Guia2_CodigoC++.cpp
@@ -54,7 +54,7 @@ int main(){
 
     return 0;
 }
-*/
+
    //2.- 
 
 class Producto{
@@ -102,5 +102,44 @@ int main(){
     for (int x=0;x<=0;x++){
         cout<<fact.P[0];
     }
+    return 0;
+}
+*/
+  //3.-
+
+class Rueda{
+    int size;
+    public:
+    Rueda(int s){
+        this->size=s;
+    }
+    int getSize(){ return size;}
+};
+class Carro{
+    Rueda rue1;  //Objetos como atributos
+    Rueda rue2;
+    Rueda rue3;
+    Rueda rue4;
+    public:
+    Carro(int r1,int r2,int r3,int r4) : rue1(r1),rue2(r2),rue3(r3),rue4(r4) {
+        Mover_carro();
+        cout<<"Size de ruedas: "<<r1<<", "<<r2<<", "<<r3<<", "<<r4<<endl;
+    } //Inicializacion de miembros objetos
+    void Mover_carro(){
+        cout<<"El carro se esta moviendo..."<<endl;
+    }
+    void Cambiar_ruedas(int ru1,int ru2,int ru3,int ru4){
+        rue1=Rueda(ru1); //El valor enviado se queda en el objeto, es una INSTANCIA de clase
+        rue2=Rueda(ru2);
+        rue3=Rueda(ru3);
+        rue4=Rueda(ru4);
+        cout<<"Se cambiaron las ruedas "<<endl;
+        cout<<"Size de ruedas: "<<rue1.getSize()<<", "<<rue2.getSize()<<", "<<rue3.getSize()<<", "<<rue4.getSize()<<endl;
+
+    }
+};
+int main(){
+    Carro car(15,15,15,15);
+    car.Cambiar_ruedas(18,18,18,18);
     return 0;
 }
