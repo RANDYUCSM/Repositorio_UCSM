62c43fa35cf9d8bc87210893ced58d7199efd071
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2024-04-02 15:40:49 -0500

    Update Guia2_CodigoC++.cpp
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- CodigosLP/Guia2_CodigoC++.cpp
+++ CodigosLP/Guia2_CodigoC++.cpp
@@ -202,3 +202,247 @@ int main(){
     cli.Pagar_cuenta(cli.getSuma());
     cout<<"Se pago la cuenta"<<endl;
 }
+
+//Ejercicio 1:
+#include <chrono>
+#include <thread>
+#include<iostream>
+using namespace std;
+
+class Auto{
+    public:
+    int velocidadA=5;
+    int velocidadPC;
+    int contador=5;
+    void setVelocidadPC(int vel){ velocidadPC=vel;}
+    int getVelocidadA(){ return velocidadA;}
+    int getVelocidadPC(){ return velocidadPC;}
+    void aumentarVelocidad(){
+        velocidadA+=5;
+        contador-=1.5;
+        cout<<"Avanzando..."<<endl;
+        this_thread::sleep_for(chrono::seconds(contador));
+        
+    }
+};
+class Carrera{
+    Auto autos;
+    int distanciaC,distanciaPC;
+    int dificultad;
+    string opcionvel;
+    public:
+    void simularCarrera(){
+        distanciaC=0;
+        distanciaPC=0;
+        if (dificultad==1){
+            autos.setVelocidadPC(20);
+            while (distanciaC<=100 and distanciaPC<=100){
+                    distanciaC+=autos.getVelocidadA();
+                    cout<<"Desea aumentar la velocidad? s/n"<<endl;
+                    cin>>opcionvel;
+                    if (opcionvel=="s" or opcionvel=="S"){
+                        autos.aumentarVelocidad();
+                    }
+                    else{
+                        cout<<"Avanzando..."<<endl;
+                        this_thread::sleep_for(chrono::seconds(autos.contador));
+                    }
+
+                    distanciaPC+=autos.getVelocidadPC();
+                    distanciaC+=autos.getVelocidadA();
+                    if(distanciaC>=100){
+                        cout<<"Ganaste!!"<<endl;
+                        break;
+                    }
+                    else if(distanciaPC>=100){
+                        cout<<"Gano la computadora"<<endl;
+                        break;
+                    }
+                    cout<<"Tu carro esta "<<distanciaC<<"/100"<<endl;
+                    cout<<"La computadora esta "<<distanciaPC<<"/100"<<endl;
+                }
+            }
+        
+        
+        else if(dificultad==2){
+            autos.setVelocidadPC(30);
+            while (distanciaC<=100 and distanciaPC<=100){
+                    distanciaC+=autos.getVelocidadA();
+                    cout<<"Desea aumentar la velocidad? s/n"<<endl;
+                    cin>>opcionvel;
+                    if (opcionvel=="s" or opcionvel=="S"){
+                        autos.aumentarVelocidad();
+                    }
+                    else{
+                        cout<<"Avanzando..."<<endl;
+                        this_thread::sleep_for(chrono::seconds(autos.contador));
+                    }
+
+                    distanciaPC+=autos.getVelocidadPC();
+                    distanciaC+=autos.getVelocidadA();
+                    if(distanciaC>=100){
+                        cout<<"Ganaste!!"<<endl;
+                        break;
+                    }
+                    else if(distanciaPC>=100){
+                        cout<<"Gano la computadora"<<endl;
+                        break;
+                    }
+                    cout<<"Tu carro esta "<<distanciaC<<"/100"<<endl;
+                    cout<<"La computadora esta "<<distanciaPC<<"/100"<<endl;
+                }
+        }
+        else if(dificultad==3){
+            autos.setVelocidadPC(40);
+            while (distanciaC<=100 and distanciaPC<=100){
+                    distanciaC+=autos.getVelocidadA();
+                    cout<<"Desea aumentar la velocidad? s/n"<<endl;
+                    cin>>opcionvel;
+                    if (opcionvel=="s" or opcionvel=="S"){
+                        autos.aumentarVelocidad();
+                    }
+                    else{
+                        cout<<"Avanzando..."<<endl;
+                        this_thread::sleep_for(chrono::seconds(autos.contador));
+                    }
+
+                    distanciaPC+=autos.getVelocidadPC();
+                    distanciaC+=autos.getVelocidadA();
+                    if(distanciaC>=100){
+                        cout<<"Ganaste!!"<<endl;
+                        break;
+                    }
+                    else if(distanciaPC>=100){
+                        cout<<"Gano la computadora"<<endl;
+                        break;
+                    }
+                    cout<<"Tu carro esta "<<distanciaC<<"/100"<<endl;
+                    cout<<"La computadora esta "<<distanciaPC<<"/100"<<endl;
+                }
+        }
+    }
+        
+    void cambiarDificultad(){
+        int d;
+        cout<<"Ingrese el nivel de dificultad "<<endl;
+        cout<<"1.Facil "<<endl;
+        cout<<"2.Medio "<<endl;
+        cout<<"3.Dificil "<<endl;
+        cin>>d;
+        dificultad=d;
+    }
+};
+int main(){
+    Carrera carrera;
+    carrera.cambiarDificultad();
+    carrera.simularCarrera();
+    return 0;
+}
+//Ejercicio 2
+class Inventario{
+    string listaProductosDisponible[10]={"cebolla","papa","arroz","azucar"};
+    string listaProductosAgotados[5]={"ajo","zanahoria","brocoli","atun","pollo"};
+    string listaPedidos[4];
+    int numproductosdisponibles=4;
+    public:
+    void AgregarProductos(){
+        int cantidadProductos;
+        string nuevo_producto;
+        cout<<"Cuantos productos desea agregar?"<<endl;
+        cin>>cantidadProductos;
+
+        for(int x=numproductosdisponibles;x<numproductosdisponibles+cantidadProductos;x++){
+            cout<<"Ingrese el nombre del producto"<<endl;
+            cin>>nuevo_producto;;
+            listaProductosDisponible[x]=nuevo_producto;
+        }
+        numproductosdisponibles+=cantidadProductos;
+    }
+    void EliminarProductos(string producto){
+        for(int x=0;x<numproductosdisponibles;x++){
+            if(listaProductosDisponible[x]==producto){
+                for(int y=x;y<numproductosdisponibles-1;y++){
+                    listaProductosDisponible[y]=listaProductosDisponible[y+1];
+                }
+                numproductosdisponibles--;
+                cout<<"Producto "<<producto<<" eliminado"<<endl;
+                return;
+            }
+        }
+        cout<<"Producto no encontrado en la lista"<<endl;
+    }
+    void ActualizarProductosD(){
+        cout<<"Los productos disponibles son: "<<endl;
+        for(int x=0;x<numproductosdisponibles;x++){
+            cout<<listaProductosDisponible[x]<<" ";
+        }
+        cout<<endl;
+    }
+    void ActualizarProductosA(){
+        cout<<"Los productos agotados son "<<listaProductosAgotados;
+        for(const auto&agotado:listaProductosDisponible){
+            cout<<agotado<<" ";
+            
+        }
+        cout<<endl;
+    }
+    void RealizarPedido(){
+        string prod;
+        int contador=0;
+        cout<<"Ingrese los 4 productos disponibles a pedir"<<endl;
+        while(contador<4){
+            cin>>prod;
+            for(const auto&item:listaProductosDisponible){
+                if(item==prod){
+                    listaPedidos[contador]=prod;
+                    contador+=1;
+                }
+            }
+        }
+    }
+    void FacturasCliente(){
+        int suma=0;
+        for (const auto&factura:listaPedidos){
+            suma+=1;
+        }
+        cout<<"El precio total de los pedidos son S/"<<suma*8<<endl; 
+    }
+
+};
+
+int main(){
+    int opc=1;
+    Inventario inventario;
+    
+    while(opc!=6){
+        cout<<"Que operacion desea hacer"<<endl;
+        cout<<"1.Agregar productos"<<endl;
+        cout<<"2.Mostrar productos"<<endl;
+        cout<<"3.Eliminar productos"<<endl;
+        cout<<"4.Realizar pedido"<<endl;
+        cout<<"5.Generar factura"<<endl;
+        cout<<"6.Salir"<<endl;
+        cin>>opc;
+        if (opc==1){
+            inventario.AgregarProductos();
+        }
+        else if(opc==2){
+            inventario.ActualizarProductosD();
+
+        }
+        else if(opc==3){
+            string eliminar;
+            cout<<"Que producto desea eliminar?"<<endl;
+            cin>>eliminar;
+            inventario.EliminarProductos(eliminar);
+
+        }
+        else if(opc==4){
+            inventario.RealizarPedido();
+        }
+        else if(opc==5){
+            inventario.FacturasCliente();
+        }
+    }
+    return 0;
+}
