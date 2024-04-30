82080e88d93c3ed9057627d2de06c595afcff7ab
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2024-04-30 00:53:03 -0500

    Update PRODUCTOS.h
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- CodigosLP/Guia5/CodigoC++/PRODUCTOS.h
+++ CodigosLP/Guia5/CodigoC++/PRODUCTOS.h
@@ -1,1 +1,32 @@
-
+#ifndef productos_h
+#define productos_h
+#include <iostream>
+using namespace std;
+class Productos{
+    private:
+    string nombre;
+    int codigo;
+    int cantidad;
+    public:
+    Productos(string n,int c,int cant): nombre(n),codigo(c),cantidad(cant){} //Constructor que recibe 3 parametros
+    Productos(int c,int cant){  //Constructor que recibe 2 parametros
+        this->nombre="Atun";
+        this->codigo=c;
+        this->cantidad=cant;
+    }
+    ~Productos(){}  //Destructor explicito
+    
+    string getNombre(){ //getters
+        return nombre;
+    }
+    int getCodigo(){
+        return codigo;
+    }
+    int getCantidad(){
+        return cantidad;
+    }
+    void setNombre(string n){nombre=n;} //setters
+    void setCodigo(int c){codigo=c;}
+    void setCantidad(int cant){cantidad=cant;}
+};
+#endif
