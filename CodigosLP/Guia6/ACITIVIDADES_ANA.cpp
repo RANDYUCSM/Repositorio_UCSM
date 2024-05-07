7b3d945a60313e756a180aa4be9c714a335216c0
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2024-05-07 17:48:03 -0500

    Update ACITIVIDADES_ANA.cpp
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- CodigosLP/Guia6/ACITIVIDADES_ANA.cpp
+++ CodigosLP/Guia6/ACITIVIDADES_ANA.cpp
@@ -99,4 +99,80 @@ int main() {
     return 0;
 }
 
+//Actividad 1 mejorado
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Estudiante {
+private:
+  string nombre;
+  int codigo;
+  float nota1;
+  float nota2;
+  float nota3;
+  float promedio;
+
+public:
+  Estudiante(string nombre, int codigo, float nota1, float nota2, float nota3) {
+    this->nombre = nombre;
+    this->codigo = codigo;
+    this->nota1 = nota1;
+    this->nota2 = nota2;
+    this->nota3 = nota3;
+    this->promedio = (nota1 + nota2 + nota3) / 3;
+  }
+
+  Estudiante() {
+    cout << "Ingrese el nombre del estudiante: ";
+    cin >> nombre;
+    cout << "Ingrese el código del estudiante: ";
+    cin >> codigo;
+    cout << "Ingrese la nota 1 del estudiante: ";
+    cin >> nota1;
+    cout << "Ingrese la nota 2 del estudiante: ";
+    cin >> nota2;
+    cout << "Ingrese la nota 3 del estudiante: ";
+    cin >> nota3;
+    this->promedio = (nota1 + nota2 + nota3) / 3;
+  }
+
+  void imprimir() {
+    cout << "Nombre: " << nombre << endl;
+    cout << "Código: " << codigo << endl;
+    cout << "Nota 1: " << nota1 << endl;
+    cout << "Nota 2: " << nota2 << endl;
+    cout << "Nota 3: " << nota3 << endl;
+    cout << "Promedio: " << promedio << endl;
+  }
+
+  float obtenerPromedio() const {
+    return promedio;
+  }
+};
+
+int main() {
+  int numero;
+  cout << "Ingrese el número de estudiantes que desea ingresar: ";
+  cin >> numero;
+
+  Estudiante estudiantes[numero];
+
+  // Ordenar los estudiantes por promedio utilizando el método de burbuja
+  for (int i = 0; i < numero - 1; i++) {
+    for (int j = 0; j < numero - i - 1; j++) {
+      if (estudiantes[j].obtenerPromedio() < estudiantes[j + 1].obtenerPromedio()) {
+        swap(estudiantes[j], estudiantes[j + 1]);
+      }
+    }
+  }
+
+  cout << "\nDatos de los estudiantes ordenados por promedio (de mayor a menor):" << endl;
+  for (int i = 0; i < numero; i++) {
+    cout << "\nEstudiante " << i + 1 << ":" << endl;
+    estudiantes[i].imprimir();
+  }
+
+  return 0;
+}
 
