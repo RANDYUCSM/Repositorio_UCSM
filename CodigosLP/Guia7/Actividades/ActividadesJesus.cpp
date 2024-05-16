3c412a8700f4e3504842259a508e44c3462cfabd
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2024-05-16 09:21:44 -0500

    Update ActividadesJesus.cpp
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- CodigosLP/Guia7/Actividades/ActividadesJesus.cpp
+++ CodigosLP/Guia7/Actividades/ActividadesJesus.cpp
@@ -1,1 +1,50 @@
+//Actividades
+#include <iostream>
+using namespace std;
 
+class Cliente{
+    protected:
+    //Datos personales clientes
+        string nombre;
+        int edad;
+    //Datos financieros
+        string contraseña;
+        string contraseña_encriptada;
+        void EncriptarContraseña(){ //Metodo para encriptar
+            for (int i=0;i<contraseña.length();i++){
+                contraseña_encriptada+=to_string(contraseña[i]);
+            }
+        }
+    public:
+        Cliente(string n,int e,string c):nombre(n),edad(e),contraseña(c){}
+
+};
+class ClienteSeguro:protected Cliente{
+    private:
+    int DNI;
+    public:
+    ClienteSeguro(string n,int e,int d,string contra):Cliente(n,e,contra),DNI(d){}
+    ~ClienteSeguro(){}; //Destructor
+    void MostrarAtributos(){
+        Cliente::EncriptarContraseña(); //Usando el metodo encriptar
+        cout<<"Nombre:"<<nombre<<endl<<"Edad:"<<edad<<endl<<"DNI:"<<DNI<<endl<<"Contrasenia:"<<contraseña_encriptada<<endl;
+    }
+};
+int main(){
+    string nombre,contra;
+    int edad,dni;
+    cout<<"Ingrese su nombre:";
+    cin>>nombre;
+    cout<<"Ingrese su edad:";
+    cin>>edad;
+    cout<<"Ingrese su DNI:";
+    cin>>dni;
+    cout<<"Ingrese su contrasenia:";
+    cin>>contra;
+    ClienteSeguro seguro(nombre,edad,dni,contra);
+    seguro.MostrarAtributos();
+
+}
+class ClienteSeguroPublic:public Cliente{};
+class ClienteSeguroProtect:protected Cliente{};
+class ClienteSeguroPrivate:private Cliente{};
