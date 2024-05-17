c19cc99eb04c1afb982b07ad63b4d845e76c5657
RANDYUCSM <randy.llerena@ucsm.edu.pe>
2024-05-17 09:36:41 -0500

    Update ActividadesRandy.cpp
    actividad
    
    Signed-off-by: RANDYUCSM <randy.llerena@ucsm.edu.pe>

--- CodigosLP/Guia7/Actividades/ActividadesRandy.cpp
+++ CodigosLP/Guia7/Actividades/ActividadesRandy.cpp
@@ -1,1 +1,69 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cliente {  // Clase base
+protected:
+    string nombre;
+    string direccion;
+    string numeroTarjeta;
+
+public:
+    Cliente(string _nombre, string _direccion, string _numeroTarjeta)
+        : nombre(_nombre), direccion(_direccion), numeroTarjeta(_numeroTarjeta) {}
+
+    virtual void mostrarInfo() {
+        cout << "Nombre: " << nombre << endl;
+        cout << "Direccion: " << direccion << endl;
+        cout << "Numero de Tarjeta: " << numeroTarjeta << endl;
+    }
+
+    virtual void encriptarInfo() {
+        cout << "Encriptando informacion " << endl;
+    }
+};
+class ClienteSeguro : public Cliente {  // Clase derivada
+private:
+    string claveSegura;
+
+public:
+    ClienteSeguro(string _nombre, string _direccion, string _numeroTarjeta, string _claveSegura): Cliente(_nombre, _direccion, _numeroTarjeta), claveSegura(_claveSegura) {}
+
+    void mostrarInfo() override {
+        Cliente::mostrarInfo();
+        cout << "Clave de Seguridad: " << claveSegura << endl;
+    }
+
+    void encriptarInfo() override {
+        cout << "Encriptando informacion" << endl;
+        string encriptada = numeroTarjeta;    //encriptacion
+        for (char& c : encriptada) {
+            if ((c)) {
+                c = ((c - '0') + 3) % 10 + '0';
+            }
+        }
+        cout << "Numero de Tarjeta encriptado: " << encriptada << endl;
+    }
+
+    void verificarAutenticidad() {
+        // Para verificar la autenticidad
+        cout << "Verificando autenticidad " << endl;
+        if (claveSegura == "ma5643") {
+            cout << "Autenticidad verificada con exito." << endl;
+        } else {
+            cout << "Clave de seguridad incorrecta." << endl;
+        }
+    }
+
+    ~ClienteSeguro() {}
+};
+
+int main() {
+    ClienteSeguro cliente("Maria suarez", "Calle 73", "1234 5678 9012 3456", "ma5643");
+    cliente.mostrarInfo();
+    cliente.encriptarInfo();
+    cliente.verificarAutenticidad();
+
+    return 0;
+}
 
