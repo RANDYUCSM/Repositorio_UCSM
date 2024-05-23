ec44a445337b5e715579a95294dc0b5e12bf0b7d
RANDYUCSM <randy.llerena@ucsm.edu.pe>
2024-05-23 10:37:59 -0500

    Update Actividades.cpp
    .
    
    Signed-off-by: RANDYUCSM <randy.llerena@ucsm.edu.pe>

--- CodigosLP/Guia8/Actividades/ActividadesRandy/Actividades.cpp
+++ CodigosLP/Guia8/Actividades/ActividadesRandy/Actividades.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
+class Animal {
+public:
+    virtual void hacerSonido()=0;
+};
+class Perro : public Animal {
+public:
+    void hacerSonido() {
+        cout << "El perro ladra" <<endl;
+    }
+};
+class Gato : public Animal {
+public:
+    void hacerSonido() {
+        cout << "El gato maulla" <<endl;
+    }
+};
+class Vaca : public Animal {
+public:
+    void hacerSonido() {
+        cout << "La vaca hace mu XD" <<endl;
+    }
+};
+int main() {
+    Animal* animal1 = new Perro();
+    Animal* animal2 = new Gato();
+    Animal* animal3 = new Vaca();
+    animal1->hacerSonido();
+    animal2->hacerSonido();
+    animal3->hacerSonido();
+
+    return 0;
+};
 
